2c118c9183054c5ed287049909e5ff56d4f2db23
Raj-panel <rajsocialpanel@gmail.com>
2026-08-05 13:18:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -658,7 +658,7 @@ function showCheckoutOverlay() {
             #checkoutPage .checkout-card { margin-bottom: 2px !important; padding: 4px 8px !important; }
             #checkoutPage .input-box { margin-bottom: 2px !important; }
             #checkoutPage input { padding: 3px 6px !important; font-size: 11px !important; height: 32px !important; }
-            #checkoutUpiView { padding: 0px !important; margin-bottom: 2px !important; text-align: center; }
+            #checkoutUpiView { padding: 0px !important; margin-bottom: 8px !important; text-align: center; }
             #checkoutUpiView img { width: 110px !important; height: 110px !important; object-fit: contain !important; margin: 0px auto !important; padding: 2px !important; border-radius: 10px !important; }
             .upi-app-btn-grid { margin-top: 4px !important; gap: 4px !important; }
             #checkoutPage .payment-tabs { margin-bottom: 2px !important; }

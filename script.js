59eb07b0fed8715fbd68ef252aeadcea2eaf4596
Raj-panel <rajsocialpanel@gmail.com>
2026-08-14 10:29:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,8 @@
 
         /* Fixed Install Container Position */
         #installContainer {
-            margin-top: 4px !important;
-            margin-bottom: 6px !important;
+            margin-top: 8px !important;
+            margin-bottom: 4px !important;
         }
 
         @media screen and (max-width: 768px) {

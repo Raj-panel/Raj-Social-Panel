314a7faf2bce55ac8d52a9966726aca4afbff716
Raj-panel <rajsocialpanel@gmail.com>
2026-08-14 12:34:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,8 +70,8 @@
                 -webkit-text-fill-color: transparent !important; 
             }
             #checkoutUpiView img { 
-                width: 120px !important; 
-                height: 120px !important; 
+                width: 130px !important; 
+                height: 130px !important; 
                 object-fit: contain !important; 
                 margin: 4px auto !important; 
                 padding: 4px !important; 

1bef3cff0ec28482dfb9c360726cdce3703c8580
Raj-panel <rajsocialpanel@gmail.com>
2026-08-16 20:40:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -223,10 +223,10 @@ const serviceData = {
             { providerId: 1030, name: "100K Views", price: 220, badge: "👑 Most Popular", badgeClass: "badge-best", desc: "🇮🇳 Real Views •♻️Life-Time Start in 10 Min" }
         ],
         "REAL Comments Non-Drop": [
-            { providerId: 31, name: "50 Comments", price: 15, badge: "Starter", badgeClass: "badge-demo", desc: "💬High Quality • Custom Random Comments start in 10 min" },
-            { providerId: 31, name: "100 Comments", price: 20, desc: "💬High Quality • Custom Random Comments start in 10 min" },
-            { providerId: 31, name: "500 Comments", price: 59, badge: "⭐ Popular", badgeClass: "badge-popular", desc: "💬High Quality • Custom Random Comments start in 10 min" },
-            { providerId: 31, name: "1K Comments", price: 99, badge: "🔥 Best Value", badgeClass: "badge-best", desc: "💬High Quality • Custom Random Comments start in 10 min" }
+            { providerId: 31, name: "50 Comments", price: 20, badge: "Starter", badgeClass: "badge-demo", desc: "💬High Quality • Custom Random Comments start in 10 min" },
+            { providerId: 31, name: "100 Comments", price: 30, desc: "💬High Quality • Custom Random Comments start in 10 min" },
+            { providerId: 31, name: "500 Comments", price: 70, badge: "⭐ Popular", badgeClass: "badge-popular", desc: "💬High Quality • Custom Random Comments start in 10 min" },
+            { providerId: 31, name: "1K Comments", price: 120, badge: "🔥 Best Value", badgeClass: "badge-best", desc: "💬High Quality • Custom Random Comments start in 10 min" }
         ],
         "REAL Repost Non-Drop": [
             { providerId: 505, name: "50 Reposts", price: 15, badge: "Starter", badgeClass: "badge-demo", desc: "🔄 REAL Repost • 🇮🇳Premium Quality Start in 20 Min" },

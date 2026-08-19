3d46da6f80140af935cadf465c9f6228ea66cb1e
Raj-panel <rajsocialpanel@gmail.com>
2026-08-19 09:05:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -174,12 +174,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const serviceData = {
     instagram: {
-        "Followers Non-Drop": [
+        /*"Followers Non-Drop": [
             {
                 type: "custom",
                 name: "Instagram Followers [High Quality] Life-time Refill♻️ -300K+ Per Day- Start in 10 Min",
                 pricePer1000: 80
-            }
+            }*/
         ],
         "Followers 30% Extra Less Drop": [
             { name: "200 Followers", price: 20, badge: "Starter", badgeClass: "badge-demo", desc: "🚀 Super Fast Delivery • Premium Quality • Starts in 2 Min" },
